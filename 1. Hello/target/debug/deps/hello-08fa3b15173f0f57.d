9f689c47ccb5224aee4c6af8883c498a87a50aef
RusProgger <ruslantimka92@gmail.com>
2025-10-06 23:57:15 +0300

    Очистка мусора и добавление файла gitignore

---
+++ 1. Hello/target/debug/deps/hello-08fa3b15173f0f57.d
@@ -0,0 +1,5 @@
+C:\Users\WhoIAm\Desktop\Rust-theory\1. Hello\target\debug\deps\hello-08fa3b15173f0f57.d: src\main.rs
+
+C:\Users\WhoIAm\Desktop\Rust-theory\1. Hello\target\debug\deps\libhello-08fa3b15173f0f57.rmeta: src\main.rs
+
+src\main.rs:
---
+++ 1. Hello/target/debug/deps/hello-2976ff6e73a80967.d
@@ -0,0 +1,5 @@
+C:\Users\WhoIAm\Desktop\Rust-theory\1. Hello\target\debug\deps\hello-2976ff6e73a80967.d: src\main.rs
+
+C:\Users\WhoIAm\Desktop\Rust-theory\1. Hello\target\debug\deps\libhello-2976ff6e73a80967.rmeta: src\main.rs
+
+src\main.rs:
