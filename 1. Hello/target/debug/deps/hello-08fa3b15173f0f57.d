868a6377d6ffb2dd10c48d83f2debf7416081c2f
RusProgger <ruslantimka92@gmail.com>
2025-10-07 00:04:58 +0300

    .gitignore: удалён target из индекса

--- 1. Hello/target/debug/deps/hello-08fa3b15173f0f57.d
+++
@@ -1,5 +0,0 @@
-C:\Users\WhoIAm\Desktop\Rust-theory\1. Hello\target\debug\deps\hello-08fa3b15173f0f57.d: src\main.rs
-
-C:\Users\WhoIAm\Desktop\Rust-theory\1. Hello\target\debug\deps\libhello-08fa3b15173f0f57.rmeta: src\main.rs
-
-src\main.rs:
