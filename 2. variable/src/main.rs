67ab4be9ab21dc8fec1fffad96dd152d070d7041
RusProgger <ruslantimka92@gmail.com>
2025-10-08 10:44:04 +0300

    Работаем с переменными и с аргументами в Rust

--- 2. variable/src/main.rs
+++ 2. variable/src/main.rs
@@ -4,7 +4,24 @@ fn main() {
     let num_code2 = 5678;
     let num_code3 = 9012;
 
+    let x = 10;
+    let y = 20;
+    let z = 30;
+
+    
+    println!("x = {2}, y = {0}, z = {1}", x, y, z);
+
+    // Аргументы 
+
+    println!("x = {1}, {1}, {1}, {2}, y = {2}, z = {0}", x, y, z);
+
+    let num: i32 = 55;
+    let x_num: i32 = 100;
+
+    println!("num = {1}, x_num = {0}", num, x_num);
+
     // выводим переменную через макрос
 
-   println!("Проверка...\nКод: {}\nПроверка...\nКод: {}\nПроверка...\nКод: {}", num_code1, num_code2, num_code3);
+    println!("Проверка...\nКод: {}\nПроверка...\nКод: {}\nПроверка...\nКод: {}", num_code1, num_code2, num_code3);
+    
 }
