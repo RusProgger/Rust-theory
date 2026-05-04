eef9f03378561c893dddd7dc74a8b68463facf31
RusProgger <ruslantimka92@gmail.com>
2026-05-04 14:58:21 +0300

    Add string rust

--- 3. string/src/main.rs
+++ 3. string/src/main.rs
@@ -1,11 +1,13 @@
 fn main() {
     println!("---------------- STRING ----------------\n");
 
-
+    // неизменяемая строка
     let str_text = String::from("Hello String");
     println!("String output: {}", str_text);
 
-    str_text.push_str(" - Hell");
+    str_text.push_str(" - Hell"); // error 
 
     println!("String output: {}", str_text);
+
+    
 }
