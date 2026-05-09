2f65da7456c6f6a2dc9b8481f6737baf1f7df5f1
RusProgger <ruslantimka92@gmail.com>
2026-05-09 11:59:09 +0300

    Add mut

--- 3. string/src/main.rs
+++ 3. string/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     println!("---------------- STRING ----------------\n");
 
     // неизменяемая строка
-    let str_text = String::from("Hello String");
+    let mut str_text = String::from("Hello String");
     println!("String output: {}", str_text);
 
     str_text.push_str(" - Hell"); // error 
