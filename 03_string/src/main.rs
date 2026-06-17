0671004f9db8187fc783a82b7fa4a31e3e72f0f0
RusProgger <ruslantimka92@gmail.com>
2026-06-18 00:49:12 +0300

    Add Use chars() to count Unicode characters in string

--- 03_string/src/main.rs
+++ 03_string/src/main.rs
@@ -71,5 +71,5 @@ fn main() {
     println!("Сообщение: {}", text_title);
 
     // Узнаем сколько символов строка
-    println!("Строка занимает кол-во {} Инеу...", text_title.len());
+    println!("Строка занимает кол-во {} byte", text_title.len());
 }
